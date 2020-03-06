f1c2e4a1dfde28edc9d7c7321ecb5c101c6883e0
guy.batton <guy.batton@duckcreek.com>
2020-03-05 22:11:19 -0600

    added queue basics

---
+++ dynamic_memory.c
@@ -0,0 +1,67 @@
+// Guy Batton
+// this is a program to make a queue that dynamically allocates and handles memory.
+
+// #include <stdlib.h> 
+// #include <limits.h> 
+// #include <stdio.h>
+
+struct LLnode {
+  char *str;
+  struct LLnode *next;
+};
+
+// adds item to end of queue.
+// returns 1 if the empty list is now not empty
+// returns 2 if adds to a non-empty list
+// returns 0 if adding fails.
+int enqueue (char *data, struct LLnode **hd, struct LLnode **tl) {
+  struct LLnode *temp;
+  temp = (struct LLnode*)malloc(sizeof(struct LLnode));
+  (*temp).str = data;
+  (*temp).next = NULL;
+  // (*temp).next = 0; // why the heck won't the linter let me assign to NULL?
+  if(!(*hd)) {
+    (*hd) = temp;
+    (*tl) = (*hd);
+    return 1;
+  } else if (*hd) {
+    (**tl).next = temp;
+    (*tl) = temp;
+    return 2;
+  } else {
+    return 0;
+  }
+}
+
+// removes the item from the front of the queue.
+// returns a pointer to a character
+char* dequeue (struct LLnode **hd, struct LLnode **tl) {
+  if ((*hd) == (*tl)) { // all the things
+    char* data = (**hd).str;
+    free(*hd); // empty the list
+    (*hd) = NULL;
+    (*tl) = NULL;
+    return data;
+  } else if ((*hd)) { // one thing
+    struct LLnode *temp;
+    temp = &hd;
+    char* data = (*temp).str;
+    (*hd) = (*temp).next;
+    free(temp); // remove the current front of queue
+    return data;
+  } else { // no thing
+    return NULL;
+  }
+} 
+
+int main() {
+  struct LLnode *head;
+  struct LLnode *tail;
+
+  head = (struct LLnode*)malloc(sizeof(struct LLnode));
+  tail = head;
+
+  
+
+  return 0;
+}
